445f72248448042b57f750276f62f03ec66a769f
oliverdial <oliverdial@localhost>
2012-04-04 11:21:33 -0400

    Made error messages in DAC communications more descriptive.  added quiet, clipboard features to smnext. added comments to smdiagpar.m added check to make sure file's not big or old in smundo.m Added protection against empty trafofn args to smrun.m changed smcATS6660v4 in a way that may be smart. Added smhendrik for bluhming code.

---
+++ sm/channels/smatrigAWG.m
@@ -0,0 +1,9 @@
+function smatrigAWG(inst)
+
+global smdata;
+
+%fprintf(smdata.inst(inst).data.inst, 'SOUR1:MARK1:VOLT:HIGH 2');
+fprintf(smdata.inst(inst).data.inst, 'SOUR1:MARK1:VOLT:LOW 2.6');
+fprintf(smdata.inst(inst).data.inst, 'SOUR3:MARK1:VOLT:LOW 2.6');
+fprintf(smdata.inst(inst).data.inst, 'SOUR1:MARK1:VOLT:LOW 0');
+fprintf(smdata.inst(inst).data.inst, 'SOUR3:MARK1:VOLT:LOW 0');
