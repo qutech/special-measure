ddbb6caebbe0de1b04fa5cf547e15724e916e630
Pascal Cerfontaine (Triton 200) <pascal.cerfontaine@rwth-aachen.de>
2017-08-22 18:09:37 +0200

    Finish driver. Sets and gets frequency and amplitude correctly via smset.

--- sm/channels/smcRigolDSG815.m
+++ sm/channels/smcRigolDSG815.m
@@ -1,34 +1,29 @@
-function [val, rate] = smcSR830(ic, val, rate)
-% [val, rate] = smcSR830(ic, val, rate, ctrl)
-% ctrl: sync (each sample triggered)
-%       trig external trigger starts acq.
-% 1: X, 2: Y, 3: R, 4: Theta, 5: freq, 6: ref amplitude
-% 7:10: AUX input 1-4, 11:14: Aux output 1:4
-% 15,16: stored data, length determined by datadim
-% 17: sensitivity
-% 18: time constant
-% 19: sync filter on/off
+function [val, rate] = smcRigolDSG815(ic, val, rate)
+% [val, rate] = smcRigolDSG815(ic, val, rate)
+% 1: Frequency in Hz
+% 2: Amplitude in dBm
 
 global smdata;
 
-
 switch ic(2) % Channel
-    case 1
+    case 1 % frequency
         switch ic(3) % action
             case 1 % set
-                
-                fprintf(smdata.inst(ic(1)).data.inst, sprintf('%s %f', cmds{ic(2)}, val));
+								fprintf(smdata.inst(ic(1)).data.inst, sprintf(':FREQ %f', val));
             case 0 % get
-                val = query(smdata.inst(ic(1)).data.inst, sprintf('%s? %s',...
-                    cmds{ic(2)}(1:4), cmds{ic(2)}(5:end)), '%s\n', '%f');
-                if ic(2)==17
-                    val = SR830sensvalue(val);
-                elseif ic(2)==18
-                    val = SR830tauvalue(val);
-                end
-
+							  val = query(smdata.inst(ic(1)).data.inst, ':FREQ?');
             otherwise
                 error('Operation not supported');
         end
+    case 2 % amplitude (level)
+        switch ic(3) % action
+            case 1 % set
+								fprintf(smdata.inst(ic(1)).data.inst, sprintf(':LEV %f', val));
+            case 0 % get
+							  val = query(smdata.inst(ic(1)).data.inst, ':LEV?');
+            otherwise
+                error('Operation not supported');
+        end
+    otherwise
+        error('Operation not supported');
 end
-
