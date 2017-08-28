87f0b193f73244edcd89f22119aee8ba4d351fb0
Rene Otten <rene.otten@rwth-aachen.de>
2017-08-28 16:11:47 +0200

    Add on/off channel to Rigol DSG815

--- sm/channels/smcRigolDSG815.m
+++ sm/channels/smcRigolDSG815.m
@@ -2,6 +2,7 @@ function [val, rate] = smcRigolDSG815(ic, val, rate)
 % [val, rate] = smcRigolDSG815(ic, val, rate)
 % 1: Frequency in Hz
 % 2: Amplitude in dBm
+% 3: Output on/off
 
 global smdata;
 
@@ -9,18 +10,28 @@ switch ic(2) % Channel
     case 1 % frequency
         switch ic(3) % action
             case 1 % set
-								fprintf(smdata.inst(ic(1)).data.inst, sprintf(':FREQ %f', val));
+                fprintf(smdata.inst(ic(1)).data.inst, sprintf(':FREQ %f', val));
             case 0 % get
-							  val = query(smdata.inst(ic(1)).data.inst, ':FREQ?');
+                val = query(smdata.inst(ic(1)).data.inst, ':FREQ?');
             otherwise
                 error('Operation not supported');
         end
     case 2 % amplitude (level)
         switch ic(3) % action
             case 1 % set
-								fprintf(smdata.inst(ic(1)).data.inst, sprintf(':LEV %f', val));
+                fprintf(smdata.inst(ic(1)).data.inst, sprintf(':LEV %f', val));
             case 0 % get
-							  val = query(smdata.inst(ic(1)).data.inst, ':LEV?');
+                val = query(smdata.inst(ic(1)).data.inst, ':LEV?');
+            otherwise
+                error('Operation not supported');
+        end
+    case 3 % output ON/OFF
+        switch ic(3)
+            case 1 % set
+                assert(val==0||val==1);
+                fprintf(smdata.inst(ic(1)).data.inst, sprintf(':OUTP %d', val));
+            case 0 % get
+                val = query(smdata.inst(ic(1)).data.inst, ':OUTP?');
             otherwise
                 error('Operation not supported');
         end
